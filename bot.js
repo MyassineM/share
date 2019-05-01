b5fa1c3a85415c09d571a945f6a7e3474ced6e05
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-05-01 08:24:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,60 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("message", (message) => {
+
+   if (message.content.startsWith("!new")) {   
+        const reason = message.content.split(" ").slice(1).join(" ");  
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات **إدارة كالفورنيا** |`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });   
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark:**إدارة كالفورنيا** | تم انشاء تذكرتك, #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, #ticket | **إدارة كالفورنيا** `)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error);
+    }
+ 
+ 
+  if (message.content.startsWith("!close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+       message.channel.send(` ``confirm`` عيد اكتب`)
+           .then((m) => {
+               message.channel.awaitMessages(response => response.content === ' confirm', {
+                       max: 1,
+                       time: 10000,
+                       errors: ['time'],
+                   })  
+                   .then((collected) => {
+                       message.channel.delete();
+                   })   
+                   .catch(() => {
+                       m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                           m2.delete();
+                       }, 3000);
+                   });
+           });
+   }
+ 
 });
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`**
-     اذا تحبني وتحب كل ناس اقبل هديتي
-وادخل ذا السيرفر عشان
-في قيف اوايات افوز فيها
-ونظام ريورد اصير غنية بيه 
-اصير اطنخ وحدة بالعالم
-بس لما تساعدني بليز ادخل
-ساعدني
-
-                                 [  https://discord.gg/enEdKV ] **`)
-}).catch(console.error)
-})
-
-client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`**
-     اذا تحبني وتحب كل ناس اقبل هديتي
-وادخل ذا السيرفر عشان
-في قيف اوايات افوز فيها
-ونظام ريورد اصير غنية بيه 
-اصير اطنخ وحدة بالعالم
-بس لما تساعدني بليز ادخل
-ساعدني
-
-                                  [  https://discord.gg/enEdKV ] **`)
-}).catch(console.error)      
-})
 client.login('NTYwMDYxODkxMDc5MDQ1MTMx.XKcqLA.Wv1WWtV9HTCc8tQjJlnNsH9Jr00');
