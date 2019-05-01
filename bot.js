53ff08bce4eaaa5d3295e27c8de023cb3b8cf2b3
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-05-01 08:45:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on("message", (message) => {
 
    if (message.content.startsWith("!new")) {   
         const reason = message.content.split(" ").slice(1).join(" ");  
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتعطي البوت برمشن ادمنسترايشن حتى يقدر يسوي الرومات ويعدل برمشنات| إدارة كالفورنيا ****`);
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
@@ -25,7 +25,7 @@ client.on("message", (message) => {
             message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, #ticket| **إدارة كالفزرنيا**`)
+                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, #ticket | **إدارة كالفورنيا**`)
                 .setTimestamp();
             c.send({
                 embed: embed
@@ -37,7 +37,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-       message.channel.send(`**إدارة كالفورنيا** | قم بتاكد عبر كتابة ``confirm```)
+       message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتب ``confirm`` `)
            .then((m) => {
                message.channel.awaitMessages(response => response.content === 'confirm', {
                        max: 1,
