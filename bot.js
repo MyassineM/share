db7ab68732806d154f29b156b02490b1e9c347c4
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-08-29 19:56:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on("message", (message) => {
 
-   if (message.content.startsWith("!new")) {   
-        const reason = message.content.split(" ").slice(1).join(" ");  
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`You need to create a rank called ``Support Team`` `);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });   
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark:  Dont we create your ticket, #${c.name}.`);
-            const embed = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  Done we create your ticket, #ticket`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-        }).catch(console.error);
+
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '436918120184021012') return;
+
+if (message.content.startsWith(prefix + '1')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + '2')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + '3')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark: : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + '4')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/MrBloods#2729");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
     }
- 
- 
-  if (message.content.startsWith("!close")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
- 
-       message.channel.send(`If you are sure write ``.confirm```)
-           .then((m) => {
-               message.channel.awaitMessages(response => response.content === '.confirm', {
-                       max: 1,
-                       time: 10000,
-                       errors: ['time'],
-                   })  
-                   .then((collected) => {
-                       message.channel.delete();
-                   })   
-                   .catch(() => {
-                       m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                           m2.delete();
-                       }, 3000);
-                   });
-           });
-   }
- 
-});
+    });	
 
   
-client.login('NjE2MjYwNTk1MDQxMzA0NTkw.XWaKug.SEDp7NICahghKhGXB-Ssa5oxWEY');
+client.login('NDM2OTE4MTIwMTg0MDIxMDEy.XRi2Yw.TeZGsmr1n9BMDhf6NvrkO-grNxw');
